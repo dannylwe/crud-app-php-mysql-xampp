066eafe570466019816967df87be07b2d4bf0e33
Daniel Lwetabe <dannylwe11@gmail.com>
2019-10-28 08:34:36 +0300

    add db connection

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+// connect to database
+$conn = mysqli_connect('localhost', 'acehanks', 'test1234', 'ninja_pizza');
+
+// check connection
+if(!$conn){
+    echo 'Connection error: ' . mysqli_connect_error();
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
