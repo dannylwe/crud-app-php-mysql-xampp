25239fffb6c825bf3c7bb942137ce3f493b995ca
Daniel Lwetabe <dannylwe11@gmail.com>
2019-10-28 09:15:12 +0300

    make assoc array

--- index.php
+++ index.php
@@ -6,6 +6,16 @@ $conn = mysqli_connect('localhost', 'acehanks', 'test1234', 'ninja_pizza');
 if(!$conn){
     echo 'Connection error: ' . mysqli_connect_error();
 }
+
+$sql = 'SELECT id, title, ingridents FROM pizzas';
+
+// make query
+$result = mysqli_query($conn, $sql);
+
+// fetch resulting records
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+print_r($pizzas);
 ?>
 
 
